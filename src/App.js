a02db2fd9289ca619203c3f63a23fc5da48ca945
zamirtcp <zamirtcp_web@users.noreply.github.com>
2020-06-22 01:09:59 +0500

    Revert "Add feature for ADD transaction"
    This reverts commit 560b98a5f31d38e03b0b29bb2a68a1cc52d423d0.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Child from './child';
-import {TransactionProvider} from './transContext';
 
 function App() {
   return (
-    <TransactionProvider>
-      <Child />
-    </TransactionProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
